06b0d7f8c579deb4a08fd95da3bc9576b506d7ec
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-06-08 02:22:41 +0530

    fix: corrected the schema for table

--- db_queries.sql
+++ db_queries.sql
@@ -1,11 +1,12 @@
 -- Schema For the Customer Contact Detials Tables
 CREATE TABLE IF NOT EXISTS CustContactDetials (
 	id INT AUTO_INCREMENT PRIMARY KEY,
+    phone_number VARCHAR(255) NOT NULL, 
 	email VARCHAR(255) NOT NULL,
     linkedId INT,
     linkPrecedence ENUM('primary', 'secondary') NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deletedAt TIMESTAMP NULL,
-    FOREIGN KEY (linkedId) REFERENCES Contacts(id)
+    FOREIGN KEY (linkedId) REFERENCES CustContactDetials(id)
 );
