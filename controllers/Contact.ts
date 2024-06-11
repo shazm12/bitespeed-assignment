e714dbf70dc57a59d8becde736afc82b8bb430ff
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-06-11 21:46:51 +0530

    chore: added createContactDetails in Contact controller

--- controllers/Contact.ts
+++ controllers/Contact.ts
@@ -1,32 +1,55 @@
 import { Request, Response } from "express";
 import { Connection } from "mysql2/typings/mysql/lib/Connection";
+import initDBConnection from "../helpers/db";
+import { error } from "console";
+import { QueryResult } from "mysql2";
+import { ContactDetails } from "../interfaces/ContactDetails";
 
-export const createContactDetail = async (
-  req: Request,
-  res: Response,
-  dbConn: any
-) => {
-  
+const getDBObj = async () => await initDBConnection().then((dbConn) => dbConn);
+
+export const createContactDetail = async (req: Request, res: Response) => {
   const { email, phoneNumber } = req.body;
-  
-  let db = await dbConn.then((db:any) => db);
 
-  const [contactDetails] =  await db.query("SELECT * FROM CustContactDetials");
+  const db = await getDBObj();
 
-  res.send(contactDetails);
+  const contactDetails = await getAllContactDetails();
+
+  let linkedId = null;
+  let error = null;
+  contactDetails.forEach((elem) => {
+
+    if(email == elem?.email && phoneNumber == elem?.phone_number ) {
+      error = "Row already present!";
+    }
+    else if(email == elem?.email || phoneNumber == elem?.phone_number) {
+      linkedId  = elem?.id;
+    }
+
+  });
+
+  if(error) {
+    res.status(400).send({ error });
+    return;
+  }
 
 
-  // await dbConn.execute(
-  //   `INSERT INTO CustContactDetials(email,linkedId,linkPrecedence,phone_number) VALUES  (?,NULL,'primary',?)`,
-  //   [email, phoneNumber],
-  //   (_err: any, result: any) => {
-  //     if (_err) {
-  //       console.log(_err);
-  //       res.status(400).send({ error: _err });
-  //       return;
-  //     }
-  //     res.status(200).send({ success: "New Customer Contact Detail Created!"});
-  //   }
-  // );
+  await db
+    .execute(
+      `INSERT INTO CustContactDetials(email,linkedId,linkPrecedence,phone_number) VALUES  (?,?,'primary',?)`,
+      [email, linkedId, phoneNumber]
+    )
+    .then((result: any) => {
+      res.status(200).send({ success: "New Customer Contact Detail Created!" });
+    })
+    .catch((error: any) => {
+      res.status(400).send({ error });
+    })
 };
 
+
+
+const getAllContactDetails = async (): Promise<ContactDetails[]> => {
+  const db = await getDBObj() ;
+  const [contactDetails] = await db.query<ContactDetails[]>("SELECT * FROM CustContactDetials");
+  return contactDetails;
+};
