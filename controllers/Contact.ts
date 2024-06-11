66d75d548fea74ffea1d79257b639360e0b9e2f6
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-06-11 22:31:07 +0530

    chore: changed logic for linkPrecedence; chore: wrote init logic for identifyContact Details;

--- controllers/Contact.ts
+++ controllers/Contact.ts
@@ -4,6 +4,7 @@ import initDBConnection from "../helpers/db";
 import { error } from "console";
 import { QueryResult } from "mysql2";
 import { ContactDetails } from "../interfaces/ContactDetails";
+import { IdentifyContactDetailsResponse } from "../interfaces/IdentifyContactDetailsResponse";
 
 const getDBObj = async () => await initDBConnection().then((dbConn) => dbConn);
 
@@ -15,14 +16,16 @@ export const createContactDetail = async (req: Request, res: Response) => {
   const contactDetails = await getAllContactDetails();
 
   let linkedId = null;
+  let linkPrecedence = "primary";
   let error = null;
   contactDetails.forEach((elem) => {
 
-    if(email == elem?.email && phoneNumber == elem?.phone_number ) {
+    if(email == elem.email && phoneNumber == elem.phone_number ) {
       error = "Row already present!";
     }
-    else if(email == elem?.email || phoneNumber == elem?.phone_number) {
-      linkedId  = elem?.id;
+    else if(email == elem.email || phoneNumber == elem.phone_number) {
+      linkedId  = elem.id;
+      linkPrecedence = "secondary";
     }
 
   });
@@ -35,8 +38,8 @@ export const createContactDetail = async (req: Request, res: Response) => {
 
   await db
     .execute(
-      `INSERT INTO CustContactDetials(email,linkedId,linkPrecedence,phone_number) VALUES  (?,?,'primary',?)`,
-      [email, linkedId, phoneNumber]
+      `INSERT INTO CustContactDetials(email,linkedId,linkPrecedence,phone_number) VALUES  (?,?,?,?)`,
+      [email, linkedId, linkPrecedence, phoneNumber]
     )
     .then((result: any) => {
       res.status(200).send({ success: "New Customer Contact Detail Created!" });
@@ -45,6 +48,53 @@ export const createContactDetail = async (req: Request, res: Response) => {
       res.status(400).send({ error });
     })
 };
+
+
+export const identifyContactDetails = async(req: Request, res: Response) => {
+
+  const { email, phoneNumber } = req.body;
+
+  const contactDetails = await getAllContactDetails();
+  let primaryContatctId = null;
+  const emails = new Set<string>();
+  const phoneNumbers = new Set<string>();
+  const secondaryContactIds = new Set<number>();
+  contactDetails.forEach((elem) => {
+
+    if(email === elem.email) {
+      emails.add(elem.email);
+      phoneNumbers.add(elem.phone_number);
+      if(elem.linkPrecedence === "primary") {
+        primaryContatctId = elem.id;
+      }
+      else {
+        secondaryContactIds.add(elem.id);
+      }
+    }
+
+    if(phoneNumber === elem.phone_number) {
+      phoneNumbers.add(elem.phone_number);
+      emails.add(elem.email);
+      if(elem.linkPrecedence === "primary") {
+        primaryContatctId = elem.id;
+      }
+      else {
+        secondaryContactIds.add(elem.id);
+      }
+    }
+  })
+
+  const contact: IdentifyContactDetailsResponse = {
+      primaryContatctId: primaryContatctId,
+      emails: Array.from(emails),
+      phoneNumbers: Array.from(phoneNumbers),
+      secondaryContactIds: Array.from(secondaryContactIds)
+  }
+
+  res.status(200).send({ contact });
+
+
+}
 
 
 
