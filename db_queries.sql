ce6a1a8a61e1ea9379788f53f4b0fba1159a38eb
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-06-13 19:29:39 +0530

    fix: correct spellings for db name;

--- db_queries.sql
+++ db_queries.sql
@@ -1,5 +1,5 @@
 -- Schema For the Customer Contact Detials Tables
-CREATE TABLE IF NOT EXISTS CustContactDetials (
+CREATE TABLE IF NOT EXISTS CustContactDetails (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     phone_number VARCHAR(255) NOT NULL, 
 	email VARCHAR(255) NOT NULL,
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS CustContactDetials (
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deletedAt TIMESTAMP NULL,
-    FOREIGN KEY (linkedId) REFERENCES CustContactDetials(id)
+    FOREIGN KEY (linkedId) REFERENCES CustContactDetails(id)
 );
