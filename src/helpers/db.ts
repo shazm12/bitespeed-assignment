2c29b1fb9a0ce6489e1058211aba23de27291640
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-06-13 20:29:56 +0530

    chore: format db file;

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -1,25 +1,26 @@
-import { ConnectionOptions } from 'mysql2';
+import { ConnectionOptions } from "mysql2";
 import dotenv from "dotenv";
-import mysql from 'mysql2/promise';
-import fs from 'fs';
+import mysql from "mysql2/promise";
+import fs from "fs";
 
 dotenv.config();
 
-const initDBConnection = async() => {
-    const connOptions: ConnectionOptions = { 
-        host: process.env.MYSQL_DB_HOST,
-        port: process.env.MYSQL_DB_PORT ? parseInt(process.env.MYSQL_DB_PORT, 10) : undefined,
-        user: process.env.MYSQL_DB_USERNAME,
-        password: process.env.MYSQL_DB_PASSWORD,
-        database: process.env.MYSQL_DB_NAME,
-        ssl: {
-            ca: fs.readFileSync(process.env.MYSQL_SSL_CA as string),
-        }
-    };
-    
-    const conn = await mysql.createConnection(connOptions);
-    return conn;
-};
+const initDBConnection = async () => {
+  const connOptions: ConnectionOptions = {
+    host: process.env.MYSQL_DB_HOST,
+    port: process.env.MYSQL_DB_PORT
+      ? parseInt(process.env.MYSQL_DB_PORT, 10)
+      : undefined,
+    user: process.env.MYSQL_DB_USERNAME,
+    password: process.env.MYSQL_DB_PASSWORD,
+    database: process.env.MYSQL_DB_NAME,
+    ssl: {
+      ca: fs.readFileSync(process.env.MYSQL_SSL_CA as string),
+    },
+  };
 
+  const conn = await mysql.createConnection(connOptions);
+  return conn;
+};
 
 export default initDBConnection;
