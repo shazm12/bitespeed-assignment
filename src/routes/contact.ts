4b915277605d1dab61f4794e288575518cd218ad
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-07-03 01:31:42 +0530

    chore: remove create endpoint;

--- src/routes/contact.ts
+++ src/routes/contact.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { createContactDetail, identifyContactDetails } from "../controllers/contact";
+import { identifyContactDetails } from "../controllers/contact";
 
 const router: Router  = Router();
 
-router.post("/create", createContactDetail);
 router.post("/identify", identifyContactDetails);
 
 
