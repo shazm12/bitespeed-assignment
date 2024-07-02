2a22e9503ef02fad3f3f7f5346d0b07b9dca3f77
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-07-02 20:52:54 +0530

    chore: remove the /api/contact route and point to root route only;

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.use("/api/contact", contactRouter);
+app.use("/", contactRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
